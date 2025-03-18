ff1b7e670e9df750a0e09691574f9c35213d719e
Tanner Kelso <tannerkelso@Mac.home>
2025-03-18 16:41:58 -0700

    Final version of API for Render deployment

--- api.py
+++ api.py
@@ -44,5 +44,5 @@ def ask_question():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Get Render's assigned port or default to 5000
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Get Render's assigned port or default to 5000
+    app.run(host="0.0.0.0", port=port)
